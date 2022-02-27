c4e4c2a14fab25d194d6d2ec4a6431aec2a5c5de
sanjeevi03 <sanjeevisan65@gmail.com>
2022-02-27 20:11:01 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const cors  = require('cors')
-const app = express()
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const mongo = require('./shared/connect');
+const {Studentsignin,signup} = require('./modules/registermodule');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+mongo.connect();
 
 app.get('/',(req,res)=>{
-   res.send("Home page")
+    res.send('Home')
 })
+app.post('/signin',Studentsignin);
+app.post('/signup',signup); 
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8000,()=>{
+    console.log("Started");
+})
